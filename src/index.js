6ad839896c3d7d79b0990a96968f85f2d64466dc
shy1008 <lokixa@naver.com>
2019-09-02 19:05:40 +0900

    0902

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 const express = require("express");
 const app = express();
 
+let users = [];
+let user = null;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 app.listen(3000);
 
 app.get("/", (req,res) => {
-    let a = 10;
-    let b = 20;
-    let c = a+b;
+    
+    res.send("hi");
+});
 
-    res.send(c.toString());
+app.get("/users", (req,res) => {
+    res.send("user"+user.name + "get");
+});
+app.get("/users/:id", (req,res) => {
+    if(user.id == req.params.id){
+        res.send("user"+user.name+"get");
+    }
+    res.send("user id"+req.params.id+"가 존재하지 않습니다.");
+});
+
+app.post("/users", (req,res) => {
+    user = req.body;
+    res.send("user add" +user.name);
+});
+
+app.put("/users/:id", (req,res) => {
+    res.send("user"+req.params.id+"edit");
+});
+
+app.delete("/users/:id", (req,res) => {
+    res.send("user"+req.params.id+"delete");
 });
